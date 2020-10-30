678b0b9c9a81429f46f5dccf5ee97c2392ae5292
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-10-30 17:34:05 -0500

    Added some Set samples

---
+++ use-Set-for-unique-object-references-in-an-array.js
@@ -0,0 +1,25 @@
+// Different JavaScript objects have different
+// object references, even with the same values:
+const someArray = [1]
+const otherArray = [1]
+// Different references even though both are [1]
+console.log(`Does someArray === someArray?`)
+console.log(`${someArray === someArray}`)
+// Output: true because of same object reference
+console.log(`Does someArray === otherArray?`)
+console.log(`${someArray === otherArray}`)
+// Output: false because of different references
+
+// We can use Set to find the unique references
+const arrays = [someArray, someArray, otherArray]
+const uniqueArrays = Array.from(new Set(arrays))
+console.log(`${uniqueArrays.length} unique refs`)
+// Output: 2 unique refs, someArray & otherArray
+
+// Example using objects instead of arrays
+const myObj = { hello: "world 🌎" }
+const moonObj = { hello: "moon 🌕" }
+const objArrays = [myObj, myObj, moonObj, moonObj]
+const uniqueObjArr = Array.from(new Set(arrays))
+console.log(`${uniqueObjArr.length} unique refs`)
+// Output: 2 unique refs, myObj & moonObj
