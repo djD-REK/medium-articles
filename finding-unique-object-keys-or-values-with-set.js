4f7df7fbfe8e77a0d7472aee5af6e1125376ebb5
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-02 19:06:14 -0500

    Unique object values with Set

---
+++ finding-unique-object-keys-or-values-with-set.js
@@ -0,0 +1,10 @@
+// You can use Object.values() with Set to find unique
+// values in an object, but keys are always unique.
+
+const myObject = { hello: "🌏", hi: "🌏", howdy: "🚯" }
+console.log(new Set(Object.keys(myObject)))
+// Output: Set(3) [ "hello", "hi", "howdy" ]
+console.log(new Set(Object.values(myObject)))
+// Output: Set [ "🌏", "🚯" ]
+console.log(new Set(Object.entries(myObject)))
+// Output: Set(3) [[ "hello", "🌏" ], ...]
--- methods-to-iterate-through-a-set.js
+++ methods-to-iterate-through-a-set.js
@@ -1,13 +1,25 @@
 const emojiSet = new Set(["🙋🏼", "🦾", "🦿", "🙋🏼"])
+
+//  Use Set.prototype.keys() or Set.prototype.values()
 const emojiIterator = emojiSet.values()
-for (const emoji of emojiIterator) { console.log(emoji) }
+// You can use a for...of loop with an Iterator object
+for (const emoji of emojiIterator) {
+  console.log(emoji)
+}
 // emojiSet.keys() is equivalent to: emojiSet.values()
-for (const emoji of emojiSet.keys()) { console.log(emoji) }
+for (const emoji of emojiSet.keys()) {
+  console.log(emoji)
+}
 // Output: "🙋🏼", "🦾", "🦿"
 
-// 1 - Use Set.prototype.keys() or Set.prototype.values()
-These two methods are equivalent; both return a// 2 - Use Set.prototype.forEach() with a Callback Function
- Docs
-Of course, you can use an arrow function as the callback function, same as you would do when working with an array.
-// 3 - Use a for...of Loop Directly With the New Set 
-As Set is a type of iterable object in JavaScript, a for...of loop will work with a Set the same way it would with an array.
+// Use Set.prototype.forEach() with a Callback Function
+emojiSet.forEach((emoji) => {
+  console.log(emoji)
+})
+// Output: "🙋🏼", "🦾", "🦿"
+
+//  Use a for...of Loop Directly With the New Set
+for (const emoji of emojiSet) {
+  console.log(emoji)
+}
+// Output: "🙋🏼", "🦾", "🦿"
