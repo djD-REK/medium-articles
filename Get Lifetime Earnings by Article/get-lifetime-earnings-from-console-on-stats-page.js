4760c29486e3c52172b82790d00f781abb9dc4ed
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-10 14:11:17 -0500

    License files & instructions

--- Get Lifetime Earnings by Article/get-lifetime-earnings-from-console-on-stats-page.js
+++ Get Lifetime Earnings by Article/get-lifetime-earnings-from-console-on-stats-page.js
@@ -1,3 +1,47 @@
+// Get Lifetime Earnings for Your Medium Articles from the Stats Page //
+
+/* Note that this software accesses the Medium's server in an automatic way that could at some point lead to your connection to Medium's server being blocked. It hasn't happened to me, but please be aware. */
+
+/* Instructions for use
+1) Copy all code found in this file
+2) Open up your Medium stats page https://medium.com/me/stats
+3) Open up your browser console (F12 key, click Console)
+4) Paste the code and hit Enter
+5) Wait (about 2 minutes)
+6) Click the drop-down for the Promise (pink text)
+7) Right-click and "Copy Message"
+8) Save to a text file (stats.txt) on your computer using a text editor such as Notepad or Visual Studio Code
+9) Delete the bottom line if it reads "debugger eval code"
+10) Open with Excel or Google Sheets (the "delimiter" is a tab character)
+11) ...
+12) Profit?
+
+*/
+
+/*
+MIT License
+
+Copyright (c) 2020 Dr. Derek Austin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 const WAIT_TIME_BEFORE_PROCESSING = 15 // increase if not reaching bottom of page
 const WAIT_TIME_BETWEEN_POSTS = 0.5 // set to 0 to not wait between posts & get results quicker
 const LOG_HOW_MANY_PENDING_MESSAGES = 10 // set to 0 to turn off time elapsed messages
